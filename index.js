ae0a117c532e62aeaf41d0ba9a759d6b76afda25
Gautam201 <g.143.p.y.com>
2024-09-01 02:16:36 +0530

    u

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const { app, server } = require("./socket/index.js");
 app.use(cors());
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"], // List of allowed methods
+    origin: "*", // List of allowed methods
     credentials: true,
   })
 );
