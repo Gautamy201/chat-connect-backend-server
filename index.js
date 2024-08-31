6ff881ca69363e32b11d97aa7923e6651cb85c16
Gautam201 <g.143.p.y.com>
2024-09-01 02:01:21 +0530

    m

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const cookieParser = require("cookie-parser");
 const { app, server } = require("./socket/index.js");
 
 // const app = express();
-app.use(cors())
+app.use(cors());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"], // List of allowed methods
     credentials: true,
   })
 );
