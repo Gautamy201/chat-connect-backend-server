9a8e383b8e9ab3b927a8b66ea7c7df30f1e9e2da
Gautam201 <g.143.p.y.com>
2024-09-02 21:59:41 +0530

    update

--- helper/deleteUploadImage.js
+++
@@ -1,9 +0,0 @@
-const deleteUploadImage = (coudniry, imgUrl) => {
-  let imgName = imgUrl.split("/").splice(-1).join().split(".")[0];
-
-  coudniry.uploader.destroy(imgName, (error, result) => {
-    console.log(error, result);
-  });
-};
-
-module.exports = deleteUploadImage;
